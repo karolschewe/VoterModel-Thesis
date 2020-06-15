1aa49f64a8f302bec163ab2d1510599ff0fc29f6
Carlito <karol.schewe@gmail.com>
2020-06-15 22:44:27 +0200

    added commuters vector initialisation

--- GminaClass.py
+++ GminaClass.py
@@ -1,9 +1,11 @@
 # GLOWNA KLASA W MODELU - EMULUJE POPULACJE GMINY WRAZ Z Z AGENTAMI ICH STANAMI I POLACZENIAMI
+from random import sample
 
 
 class GminaClass:
     _teryt_ = ""
     voters_states = [] # wektor opinii glosujacych True = konserwatyzm, False = antykonserwatyzm]
+    working_gmina = [] # wektor miejsca pracy glosujacych; przyjmuje on wartosc terytu gminy przyjazdu lub "S" dla tej samej gminy
 
     # funkcja definiujaca wektor glosujacych
 
@@ -14,8 +16,41 @@ class GminaClass:
 
     def __init__(self, teryt_code: str, population: int, conservatism_support: float, downscale_factor: int):
         self._teryt_ = teryt_code
-        pop_size = round(population/downscale_factor)
-        self.voters_states = self.define_voters_vec(size=pop_size, cons_supp=conservatism_support)
+        self.factor = downscale_factor
+        self.n_agents = round(population/downscale_factor)
+        self.voters_states = self.define_voters_vec(size=self.n_agents, cons_supp=conservatism_support)
+
+    def initialize_workplaces(self,df):
+        # wybranie wierszy dotyczacych naszej gminy
+        our_gmina = df['FROM'] == self._teryt_
+        where_to = df[our_gmina]
+        # wyznaczenie po ile bedzie dojezdzalo po zeskalowaniu w sumie
+        sum_commuters = 0
+        # wyznaczenie po ile bedzie dojezdzalo do kazdej gminy
+        how_many_commuters = {}
+        for idx, row in where_to.iterrows():
+            nn = round(row['n']/self.factor)
+            sum_commuters = sum_commuters + nn
+            how_many_commuters[row['TO']] = nn # indeks w slowniku jest oznaczeniem gminy przyjazdu, wartosc oznacza liczbe agentow
+        # wylosowanie ktorzy beda dojezdzali
+        indexes = sample(range(self.n_agents),sum_commuters)
+        # inicjalizacja wartoscia poczatkowa
+        self.working_gmina = ['S'] * self.n_agents
+
+        # zamiana pojedynczych wartosci
+        i = 0
+        for idx, value in how_many_commuters.items():
+            for j in range(value):
+                self.working_gmina[indexes[i]] = idx
+                i = i+1
+
+
+
+
+
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # VoterModel-Thesis
-# Voter Model created for my master degree.
+####Voter Model created for my master degree.
+
+#####Main assumptions of the model.
+1. In Poland smallest administrative division is gmina
+2. Within each gmina a metapopulation of agents is created.
+3. Each agent has two possible political opinions:
+
+a) conservatism - represented with value True
+
+b) anticonservatism - represented with False
+
+4. Within each gmina every agent is connected with every other agents.
+5. Based on statistical data, a portion of people that commute to work to other gminas have also connection to agents inside the gmina they commute to.
+6. Connected agents can exchange their political opinion with probability p in each step.
+
+
+GminaClass is the main class of the project. It represents the metapopulation within a Gmina.
+Each instance contains its TERYT code - which is official polish gmina ID.
+Moreover each GminaClass contains 2 vectors of Boolean values, first representing opinions of agents and the other 
+representing gminas ID where they commute to.
+
